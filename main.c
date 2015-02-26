e9d9322a6035061cded1366dfc69787a81ac768b
rigon <ricar.goncalves@gmail.com>
2015-02-26 03:56:59 +0000

    Parser

---
+++ main.c
@@ -0,0 +1,124 @@
+/*
+ * LogFormat "%v:%p %h %l %u %t \"%r\" %>s %O \"%{Referer}i\" \"%{User-Agent}i\"" vhost_combined
+ * LogFormat "%h %l %u %t \"%r\" %>s %O \"%{Referer}i\" \"%{User-Agent}i\"" combined
+ * LogFormat "%h %l %u %t \"%r\" %>s %O" common
+ * LogFormat "%{Referer}i -> %U" referer
+ * LogFormat "%{User-agent}i" agent
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define LINE_WIDTH	10000
+
+typedef struct {
+	char *ip;
+	char *identd;
+	char *user;
+	char *time;
+	char *request;
+	char *status_code;
+	char *size;
+	char *referer;
+	char *user_agent;
+} REQUEST;
+
+
+char *next_parameter(char *buf, char ending, char *last) {
+	if(buf >= last) return NULL;
+	
+	int i=0;
+	
+	for(i=0; buf[i] != ending && buf[i] != '\0' && i < LINE_WIDTH; i++);
+	buf[i] = '\0';
+	return &buf[i+1];
+}
+
+void process_line(char *line) {
+	
+	REQUEST r;
+	
+	char *pos = line;
+	char *last = &line[LINE_WIDTH];
+	
+	// IP
+	r.ip = pos;
+	pos = next_parameter(pos, ' ', last);
+	
+	// identd
+	r.identd = pos;
+	pos = next_parameter(pos, ' ', last);
+	
+	// User
+	r.user = pos;
+	pos = next_parameter(pos, ' ', last);
+	
+	// Time
+	pos++;	// Skip '['
+	r.time = pos;
+	pos = next_parameter(pos, ']', last);
+	pos++;
+	
+	// Request
+	pos++;	// Skip '"'
+	r.request = pos;
+	pos = next_parameter(pos, '"', last);
+	pos++;
+	
+	// Status Code
+	r.status_code = pos;
+	pos = next_parameter(pos, ' ', last);
+	
+	// Size
+	r.size = pos;
+	pos = next_parameter(pos, ' ', last);
+	
+	// Referer
+	pos++; 	// Skip '"'
+	r.referer =pos;
+	pos = next_parameter(pos, '"', last);
+	pos++;
+	
+	// User Agent
+	pos++; 	// Skip '"'
+	r.user_agent = pos;
+	pos = next_parameter(pos, '"', last);
+	pos++;
+	
+	
+	printf("%s\nIP: %s\nIdentd: %s\nUser: %s\nTime: %s\nRequest: %s\nStatus Code: %s\nSize: %s\nReferer: %s\nUser Agent: %s\n\n",
+		line, r.ip, r.identd, r.user, r.time, r.request, r.status_code, r.size, r.referer, r.user_agent);
+}
+
+
+
+int main(int argc, char **argv) {
+	
+	// Arguments
+	if(argc != 2) {
+		fprintf(stderr, "Wrong number of arguments!\n");
+		fprintf(stderr, "Usage: %s apache_log_file\n",argv[0]);
+		return EXIT_FAILURE;
+	}
+	
+	// Open log
+	FILE *log = fopen(argv[1], "rt");
+	if(log == NULL) {
+		fprintf(stderr, "Cannot open log file '%s'\n", argv[1]);
+		return EXIT_FAILURE;
+	}
+	
+	// Reads the log file
+	char buf[LINE_WIDTH];
+	int i=0;
+	while( fgets(buf, LINE_WIDTH, log) != NULL ) {
+		process_line(buf);
+	
+		printf("%d\n", ++i);
+	}
+	
+	// Close log
+	fclose(log);
+	
+	return EXIT_SUCCESS;
+}
